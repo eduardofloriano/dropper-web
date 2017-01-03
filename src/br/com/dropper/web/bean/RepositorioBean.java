ab6b744a12fe892899479514474f9ad5de108f96
Eduardo <eduardofloriano@outlook.com>
2017-01-03 20:51:24 -0200

    Adicionando Logos, Adicionando Grafico do repositorio.

--- src/br/com/dropper/web/bean/RepositorioBean.java
+++ src/br/com/dropper/web/bean/RepositorioBean.java
@@ -8,9 +8,17 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.PieChartModel;
 
+import br.com.dropper.web.dao.ArquivoDAO;
+import br.com.dropper.web.dao.AudioDAO;
+import br.com.dropper.web.dao.ImagemDAO;
 import br.com.dropper.web.dao.RepositorioDAO;
+import br.com.dropper.web.dao.VideoDAO;
 import br.com.dropper.web.model.Usuario;
 import br.com.dropper.web.util.JpaUtil;
 
@@ -28,13 +36,14 @@ public class RepositorioBean implements Serializable {
 	private Usuario usuario = new Usuario();
 	private Usuario usuarioLogado = new Usuario();
 	private PieChartModel repositorioPieChartModel;
-	
+	private BarChartModel repositorioBarModel;
 
 	@PostConstruct
-    public void init() {
-        createPieModel();
-    }
-	
+	public void init() {
+		createPieModel();
+		createBarModel();
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -42,7 +51,7 @@ public class RepositorioBean implements Serializable {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
+
 	public PieChartModel getRepositorioPieChartModel() {
 		return repositorioPieChartModel;
 	}
@@ -51,17 +60,24 @@ public class RepositorioBean implements Serializable {
 		this.repositorioPieChartModel = repositorioPieChartModel;
 	}
 
+	public BarChartModel getRepositorioBarModel() {
+		return repositorioBarModel;
+	}
+
+	public void setRepositorioBarModel(BarChartModel repositorioBarModel) {
+		this.repositorioBarModel = repositorioBarModel;
+	}
+
 	public Usuario getUsuarioLogado() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		this.usuarioLogado = (Usuario) context.getExternalContext().getSessionMap().get("usuarioLogado");
 		return this.usuarioLogado;
 	}
 
-	
 	public Long getEspacoDisponivel() {
 		if (getUsuarioLogado() != null) {
 			Long espacoTotal = usuarioLogado.getRepositorio().getEspacoTotal();
-			
+
 			Long espacoOcupado = repositorioDAO.obterEspacoOcupadoImagemPorUsuario(usuarioLogado)
 					+ repositorioDAO.obterEspacoOcupadoArquivoPorUsuario(usuarioLogado)
 					+ repositorioDAO.obterEspacoOcupadoVideoPorUsuario(usuarioLogado)
@@ -72,26 +88,69 @@ public class RepositorioBean implements Serializable {
 		return 1L;
 	}
 
-	
 	private void createPieModel() {
-		repositorioPieChartModel = new PieChartModel();        
-		
-		if(getUsuarioLogado() != null){
-			Long espacoTotal = usuarioLogado.getRepositorio().getEspacoTotal();		
-			
+		repositorioPieChartModel = new PieChartModel();
+
+		if (getUsuarioLogado() != null) {
+			Long espacoTotal = usuarioLogado.getRepositorio().getEspacoTotal();
+
 			repositorioPieChartModel.set("Imagens", repositorioDAO.obterEspacoOcupadoImagemPorUsuario(usuarioLogado));
 			repositorioPieChartModel.set("Arquivos", repositorioDAO.obterEspacoOcupadoArquivoPorUsuario(usuarioLogado));
 			repositorioPieChartModel.set("Videos", repositorioDAO.obterEspacoOcupadoVideoPorUsuario(usuarioLogado));
 			repositorioPieChartModel.set("Audios", repositorioDAO.obterEspacoOcupadoAudioPorUsuario(usuarioLogado));
-	         
+
 			repositorioPieChartModel.setTitle("Repositorio");
 			repositorioPieChartModel.setLegendPosition("e");
 			repositorioPieChartModel.setFill(false);
 			repositorioPieChartModel.setShowDataLabels(true);
 			repositorioPieChartModel.setDiameter(150);
 		}
+
+	}
+
+	private void createBarModel() {
+		
+		repositorioBarModel = new BarChartModel();
+
+		if (getUsuarioLogado() != null) {
+			
+			
+			ChartSeries imagens = new ChartSeries();
+			imagens.setLabel("Imagens");
+			imagens.set("Imagens", new ImagemDAO(em).obterImagensPorUsuario(usuarioLogado).size());
+
+			ChartSeries arquivos = new ChartSeries();
+			arquivos.setLabel("Arquivos");
+			arquivos.set("Arquivos", new ArquivoDAO(em).obterArquivosPorUsuario(usuarioLogado).size());
+
+			ChartSeries videos = new ChartSeries();
+			videos.setLabel("Videos");
+			videos.set("Videos", new VideoDAO(em).obterVideosPorUsuario(usuarioLogado).size());
+
+			ChartSeries audios = new ChartSeries();
+			audios.setLabel("Audios");
+			audios.set("Audios", new AudioDAO(em).obterAudiosPorUsuario(usuarioLogado).size());
+
+			repositorioBarModel.addSeries(imagens);
+			repositorioBarModel.addSeries(arquivos);
+			repositorioBarModel.addSeries(videos);
+			repositorioBarModel.addSeries(audios);
+			
+		
+			repositorioBarModel.setTitle("Quantidade de Arquivos");
+			repositorioBarModel.setLegendPosition("ne");
+
+			Axis xAxis = repositorioBarModel.getAxis(AxisType.X);
+//			xAxis.setLabel("Arquivos Multimídia");
+
+			Axis yAxis = repositorioBarModel.getAxis(AxisType.Y);
+//			yAxis.setLabel("Quantidade de Arquivos");
+			yAxis.setMin(0);
+			//yAxis.setMax(200);
+			
+		}
 		
 		
-    }
-	
+	}
+
 }
